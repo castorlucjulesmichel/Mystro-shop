30489edcd3f4bd928e50bd23ff1531d0852cd46d
castorlucjulesmichel <castormystro@gmail.com>
2026-09-15 06:51:09 -0400

    Refresh cache for persistent admin history

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="mystro-shop-v24";
+const CACHE_NAME="mystro-shop-v25";
 const CORE_FILES=["./","./index.html","./style.css","./script.js","./checkout.html","./admin.html","./admin.css","./admin-extra.css","./admin.js","./admin-ui-v4.js","./admin-lazy-loader.js","./admin-users-v2.js","./admin-approvals-lite.js","./admin-wallet-history-lite.js","./admin-chat-button.js","./account-control.js","./order-admin.js","./live-fixes.js","./manifest.json","./icon-192.png","./icon-512.png","./mobile-fix.css","./ui-fix.js"];
 
 self.addEventListener("install",event=>{
