cb9b8300d701a4f43847bb0cad2a90adcdea9d63
castorlucjulesmichel <castormystro@gmail.com>
2026-09-23 19:50:09 -0400

    Create Mystro Connect app

---
+++ mystro-connect/firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyC3JebExbgH1n40wzpwNjtASmOPG1tuKIs",
+  authDomain: "mystroshop-eab92.firebaseapp.com",
+  projectId: "mystroshop-eab92",
+  storageBucket: "mystroshop-eab92.firebasestorage.app",
+  messagingSenderId: "104073035061",
+  appId: "1:104073035061:web:59d2779f2db7a8a3be207c"
+};
